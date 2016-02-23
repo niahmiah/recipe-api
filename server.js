5cf5341f0166cde578724f077fbddfc6adc6a780
Ian Patton <ian.patton@gmail.com>
2016-02-23 18:24:40 -0500

    cors

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 'use strict';
-const useSSL = false;
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./lib/routes');
 const DB = 'mongodb://localhost:27017/mealplanner';
 const PORT = 3000;
-if (useSSL) {
-  const key = fs.readFileSync('./privkey.pem');
-  const cert = fs.readFileSync('./cert.pem')
-  const https_options = {
-    key,
-    cert
-  };
-}
 
 let server;
 
@@ -26,12 +15,12 @@ const start = (cb) => {
   db.on('error', cb);
   db.once('open', () => {
     const app = express();
+    app.use(cors());
+    app.options('*', cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded());
-    app.use(cors());
     app.use(routes);
-    if (useSSL) { server = https.createServer(https_options, app).listen(PORT, cb); }
-    else { server = app.listen(PORT, cb); }
+    server = app.listen(PORT, cb);
   });
 };
 
